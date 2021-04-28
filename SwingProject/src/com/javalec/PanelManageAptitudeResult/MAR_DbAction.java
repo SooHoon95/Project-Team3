bbcf53dc598d601472e9ac7961106c3cb93f5369
Ray-Ari <80452660+Ray-Ari@users.noreply.github.com>
2021-04-28 14:23:59 +0900

    MAR_Dbaction

---
+++ SwingProject/src/com/javalec/PanelManageAptitudeResult/MAR_DbAction.java
@@ -0,0 +1,21 @@
+package com.javalec.PanelManageAptitudeResult;
+
+import com.javalec.Datadefine.data_Enviroment_define;
+
+public class MAR_DbAction {
+
+	private final String url_mysql = data_Enviroment_define.url_mysql;
+	private final String id_mysql = data_Enviroment_define.id_mysql;
+	private final String pw_mysql = data_Enviroment_define.pw_mysql;
+	
+	//Field 
+	
+	
+	//Construction
+	public MAR_DbAction() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Method
+	
+}
