c68ed9460c6ebb1f9f6caa6c514063b071c48afb
Ray-Ari <80452660+Ray-Ari@users.noreply.github.com>
2021-04-28 14:58:25 +0900

    DB수정

--- Database/SwingDatabase.sql
+++ Database/SwingDatabase.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: swing_project_team3
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -40,6 +40,60 @@ INSERT INTO `admin` VALUES ('admin','1234');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `aptitudequestion`
+--
+
+DROP TABLE IF EXISTS `aptitudequestion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aptitudequestion` (
+  `aqNum` int(11) NOT NULL,
+  `aqQuestion` varchar(45) DEFAULT NULL,
+  `aqAnswer1` varchar(10) DEFAULT NULL,
+  `aqAnswer2` varchar(10) DEFAULT NULL,
+  `aqScore1` varchar(10) DEFAULT NULL,
+  `aqScore` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`aqNum`),
+  UNIQUE KEY `qqNum_UNIQUE` (`aqNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aptitudequestion`
+--
+
+LOCK TABLES `aptitudequestion` WRITE;
+/*!40000 ALTER TABLE `aptitudequestion` DISABLE KEYS */;
+INSERT INTO `aptitudequestion` VALUES (1,'1','1','2','1','0'),(2,'2','1','2','0','1');
+/*!40000 ALTER TABLE `aptitudequestion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aptituderesult`
+--
+
+DROP TABLE IF EXISTS `aptituderesult`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aptituderesult` (
+  `arNum` int(11) NOT NULL,
+  `arName` varchar(45) DEFAULT NULL,
+  `arExplain` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`arNum`),
+  UNIQUE KEY `QuizNum_UNIQUE` (`arNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aptituderesult`
+--
+
+LOCK TABLES `aptituderesult` WRITE;
+/*!40000 ALTER TABLE `aptituderesult` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aptituderesult` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `mbtiquestion`
 --
 
@@ -47,7 +101,7 @@ DROP TABLE IF EXISTS `mbtiquestion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `mbtiquestion` (
-  `mqNum` int NOT NULL AUTO_INCREMENT,
+  `mqNum` int(11) NOT NULL AUTO_INCREMENT,
   `mqQuestion` varchar(45) DEFAULT NULL,
   `mqType` varchar(10) DEFAULT NULL,
   `mqAnswer1` varchar(10) DEFAULT NULL,
@@ -77,13 +131,12 @@ DROP TABLE IF EXISTS `mbtiresult`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `mbtiresult` (
-  `mbtiNum` int NOT NULL AUTO_INCREMENT,
-  `mbtiType` varchar(10) DEFAULT NULL,
-  `mbtiName` varchar(12) DEFAULT NULL,
-  `mbtiExplain` varchar(1000) DEFAULT NULL,
-  `mbtiImage` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`mbtiNum`),
-  UNIQUE KEY `MbtiNum_UNIQUE` (`mbtiNum`)
+  `mrNum` int(11) NOT NULL AUTO_INCREMENT,
+  `mrType` varchar(10) DEFAULT NULL,
+  `mrName` varchar(12) DEFAULT NULL,
+  `mrExplain` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`mrNum`),
+  UNIQUE KEY `MbtiNum_UNIQUE` (`mrNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -97,63 +150,6 @@ LOCK TABLES `mbtiresult` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `quizquestion`
---
-
-DROP TABLE IF EXISTS `quizquestion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `quizquestion` (
-  `qqNum` int NOT NULL AUTO_INCREMENT,
-  `qqQuestion` varchar(45) DEFAULT NULL,
-  `qqAnswer1` varchar(10) DEFAULT NULL,
-  `qqAnswer2` varchar(10) DEFAULT NULL,
-  `qqScore1` varchar(10) DEFAULT NULL,
-  `qqScore` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`qqNum`),
-  UNIQUE KEY `qqNum_UNIQUE` (`qqNum`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `quizquestion`
---
-
-LOCK TABLES `quizquestion` WRITE;
-/*!40000 ALTER TABLE `quizquestion` DISABLE KEYS */;
-INSERT INTO `quizquestion` VALUES (1,'1','1','2','1','0'),(2,'2','1','2','0','1');
-/*!40000 ALTER TABLE `quizquestion` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `quizresult`
---
-
-DROP TABLE IF EXISTS `quizresult`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `quizresult` (
-  `quizNum` int NOT NULL AUTO_INCREMENT,
-  `quizType` varchar(45) DEFAULT NULL,
-  `quizExplain` varchar(45) DEFAULT NULL,
-  `quizGrade1` varchar(45) DEFAULT NULL,
-  `quizGrade2` varchar(45) DEFAULT NULL,
-  `quizGrade3` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`quizNum`),
-  UNIQUE KEY `QuizNum_UNIQUE` (`quizNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `quizresult`
---
-
-LOCK TABLES `quizresult` WRITE;
-/*!40000 ALTER TABLE `quizresult` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quizresult` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
@@ -161,7 +157,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `userNum` int NOT NULL AUTO_INCREMENT,
+  `userNum` int(11) NOT NULL AUTO_INCREMENT,
   `userName` varchar(15) DEFAULT NULL,
   `userId` varchar(45) DEFAULT NULL,
   `userPw` varchar(45) DEFAULT NULL,
@@ -192,4 +188,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-28 13:58:10
+-- Dump completed on 2021-04-28 14:56:49
